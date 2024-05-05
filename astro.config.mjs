89802ae0292054ba8d09986b71dfa6eb6141bc07
momen salama <momensalama712@gmail.com>
2024-05-05 18:02:10 +0300

    Update Astro config to change site property 🔧

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,19 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import solidJs from '@astrojs/solid-js';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-ecommerce.vercel.app',
-  output: 'hybrid',
-  integrations: [react({
-    include: ['**/react/*']
-  }), solidJs({
-    include: ['**/solid/*']
-  })],
-  adapter: vercel()
+	site: 'https://astro-web-app-gamma.vercel.app',
+	output: 'hybrid',
+	integrations: [
+		react({
+			include: ['**/react/*'],
+		}),
+		solidJs({
+			include: ['**/solid/*'],
+		}),
+	],
+	adapter: vercel(),
 });
