d302f721a5f75ad50ff242b7fe38c2d51118ff58
momen salama <momensalama712@gmail.com>
2024-05-05 15:19:48 +0300

    Update Astro config and add newsletter feature ✨

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import solidJs from '@astrojs/solid-js';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://astro-ecommerce.vercel.app',
+	output: 'hybrid',
 	integrations: [
 		react({
 			include: ['**/react/*'],
